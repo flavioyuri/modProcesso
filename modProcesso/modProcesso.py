07b3ec2b734fb580e4e136f094f4bfe26a02ef24
Flavio Yuri <40678117+flavioyuri@users.noreply.github.com>
2023-10-21 09:48:01 -0300

    Nova correção

--- modProcesso/modProcesso.py
+++ modProcesso/modProcesso.py
@@ -1305,8 +1305,8 @@ def tabelamentoPorFrequencia(event_log, df, p, list_test, df_test, minimo=3, max
       #min = dfaToNfa(minJoinFalse)
       #nfaResultado = operacaoSequencias(min, minimo, maximo)
       #fit = fitnessAutomata(nfaResultado, list_test, True, True)
-      print([f"{p*100}% mais frequentes",len(nfaResultado.alphabet),len(nfaResultado.states),len(nfaResultado.transition),len(nfaResultado.acceptStates), len(nfaResultado.NFAs), nfaResultado.len_states(), tkb['perc_fit_traces'], simp])
-      resultados.append([f"{p*100}% mais frequentes",len(nfaResultado.alphabet),len(nfaResultado.states),len(nfaResultado.transition),len(nfaResultado.acceptStates), len(nfaResultado.NFAs), nfaResultado.len_states(), tkb['perc_fit_traces'], simp])
+      print([f"{p*100}% mais frequentes",len(minJoinFalse.alphabet),len(minJoinFalse.states),len(minJoinFalse.transition),len(minJoinFalse.acceptStates), len(minJoinFalse.NFAs), minJoinFalse.len_states(), tkb['perc_fit_traces'], simp])
+      resultados.append([f"{p*100}% mais frequentes",len(minJoinFalse.alphabet),len(minJoinFalse.states),len(minJoinFalse.transition),len(minJoinFalse.acceptStates), len(minJoinFalse.NFAs), minJoinFalse.len_states(), tkb['perc_fit_traces'], simp])
       p = p+0.05
     return resultados
 
